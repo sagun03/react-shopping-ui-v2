b4aae2e979049c87b858b7326f4ca406af54af6a
sagun03 <sagunsaluja13@gmail.com>
2024-10-01 17:49:21 -0400

    updated screen for reviews, navigation button and product page

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -14,7 +14,11 @@ import {
   RatingSection,
   RatingBreakdown,
   StyledList,
-  StyledListItem
+  StyledListItem,
+  RatingWrapper,
+  CustomDivider,
+  Wrapper,
+  RatingContainer
 } from "../components/styles/Review";
 import { useReviews } from "../hooks/useReview";
 
@@ -22,7 +26,112 @@ const Review = ({ productId }) => {
   const [reviews, setReviews] = useState([]);
   const [averageRating, setAverageRating] = useState(0);
   const [ratingCounts, setRatingCounts] = useState([0, 0, 0, 0, 0]);
-
+  const review = [
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    },
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    },
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    },
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    },
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    },
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    },
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    },
+    {
+      _id: "66bd261992d427cdaec24ef5",
+      userID: "507f1f77bcf86cd799439013",
+      productId: "66a95400ab4a781270bab637",
+      rating: 3,
+      description: "I really loved this product. It exceeded my expectations.",
+      title: "Average product!",
+      name: "Chaman",
+      createdAt: "2024-08-14T12:34:56.000Z",
+      isApproved: false,
+      updatedAt: "2024-08-14T21:48:09.618Z",
+      __v: 0
+    }
+  ]
   const {
     data: reviewData,
     isLoading: isReviewsLoading,
@@ -64,96 +173,111 @@ const Review = ({ productId }) => {
       </ReviewContainer>
     );
   }
-
   return (
     <ReviewContainer>
-      <Typography variant="h4" sx={{ fontWeight: "bold" }} gutterBottom>
+
+      <Typography
+        variant="h4"
+        sx={{ fontWeight: "bold", fontSize: "2.5rem" }}
+        gutterBottom
+      >
         Customer Reviews
       </Typography>
-      {reviews.length > 0 ? (
-        <>
-          <RatingSection>
-            <Typography variant="h5">
-              Overall Rating: {averageRating.toFixed(1)}
-            </Typography>
-            <Rating
-              value={averageRating}
-              precision={0.1}
-              readOnly
-              sx={{ ml: 1 }}
-            />
-            <Typography variant="h6" sx={{ ml: 1 }}>
-              {getRatingLabel(averageRating)}
-            </Typography>
-          </RatingSection>
-          <Divider sx={{ my: 2 }} />
-          <RatingBreakdown>
-            {ratingCounts.map((count, index) => (
-              <Box key={index} sx={{ display: "flex", alignItems: "center" }}>
-                <Typography variant="body2">{5 - index} star</Typography>
-                <LinearProgress
-                  variant="determinate"
-                  value={count > 0 ? (count / reviews.length) * 100 : 0}
-                  sx={{ mx: 1, flex: 1 }}
+      <Wrapper>
+      <RatingContainer>
+        <RatingWrapper>
+          {reviews.length > 0 ? (
+            <>
+              <RatingSection>
+                <Typography variant="h4">Overall Rating</Typography>
+                <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                  {averageRating.toFixed(1)}
+                </Typography>
+                <Rating
+                  value={averageRating}
+                  precision={0.1}
+                  readOnly
+                  size="large"
+                  sx={{ ml: 1, color: "green" }}
                 />
-                <Typography variant="body2">{count}</Typography>
-              </Box>
-            ))}
-          </RatingBreakdown>
-        </>
-      ) : isReviewsLoading ? (
-        <Typography variant="body1" sx={{ padding: "10px" }}>
-          Loading reviews...
-        </Typography>
-      ) : (
-        <Typography variant="body1" sx={{ padding: "10px" }}>
-          No reviews yet. Be the first to review this product!
-        </Typography>
-      )}
-      <StyledList>
-        {reviews.map((review, index) => (
-          <StyledListItem key={index}>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-              <Avatar src={review.photoUrl} alt={review.name} />
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-start"
-                }}
-              >
-                <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-                  {review.title}
+                <Typography variant="h6" sx={{ ml: 1 }}>
+                  {getRatingLabel(averageRating)}
                 </Typography>
-                <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
-                  {review.rating.toFixed(1)}
-                  <Rating
-                    value={review.rating}
-                    precision={0.5}
-                    readOnly
-                    size="small"
-                    sx={{ ml: 0.5 }}
-                  />
-                </Box>
-                <Typography
-                  variant="subtitle2"
-                  sx={{ fontWeight: "500", mt: 0.5 }}
-                >
-                  {review.name} •{" "}
-                  {new Date(review.createdAt).toLocaleDateString()}
-                </Typography>
-              </Box>
-            </Box>
-            <Typography
-              variant="body1"
-              sx={{ marginTop: "0.5rem", marginLeft: "0.5rem" }}
-            >
-              {review.description}
+              </RatingSection>
+              <CustomDivider orientation="vertical" />
+              <RatingBreakdown>
+                {ratingCounts.map((count, index) => (
+                  <Box
+                    key={index}
+                    sx={{ display: "flex", alignItems: "center" }}
+                  >
+                    <Typography variant="body2">{5 - index} star</Typography>
+                    <LinearProgress
+                      variant="determinate"
+                      value={count > 0 ? (count / reviews.length) * 100 : 0}
+                      sx={{ mx: 1, flex: 1 }}
+                    />
+                    <Typography variant="body2">{count}</Typography>
+                  </Box>
+                ))}
+              </RatingBreakdown>
+            </>
+          ) : isReviewsLoading ? (
+            <Typography variant="body1" sx={{ padding: "10px" }}>
+              Loading reviews...
             </Typography>
-            <Divider sx={{ mt: 2 }} />
-          </StyledListItem>
-        ))}
-      </StyledList>
+          ) : (
+            <Typography variant="body1" sx={{ padding: "10px" }}>
+              No reviews yet. Be the first to review this product!
+            </Typography>
+          )}
+        </RatingWrapper>
+        </RatingContainer>
+        <StyledList>
+          {review.map((review, index) => (
+            <StyledListItem key={index}>
+              <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                <Avatar src={review.photoUrl} alt={review.name} />
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start"
+                  }}
+                >
+                  <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                    {review.title}
+                  </Typography>
+                  <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+                    {review.rating.toFixed(1)}
+                    <Rating
+                      value={review.rating}
+                      precision={0.5}
+                      readOnly
+                      size="small"
+                      sx={{ ml: 0.5 }}
+                    />
+                  </Box>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{ fontWeight: "500", mt: 0.5 }}
+                  >
+                    {review.name} •{" "}
+                    {new Date(review.createdAt).toLocaleDateString()}
+                  </Typography>
+                </Box>
+              </Box>
+              <Typography
+                variant="body1"
+                sx={{ marginTop: "0.5rem", marginLeft: "0.5rem" }}
+              >
+                {review.description}
+              </Typography>
+              <Divider sx={{ mt: 2 }} />
+            </StyledListItem>
+          ))}
+        </StyledList>
+      </Wrapper>
     </ReviewContainer>
   );
 };
